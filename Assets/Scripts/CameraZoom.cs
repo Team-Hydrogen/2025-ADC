282620313cba4a912c8a4802cd0cd48fef63c704
Aarin Dave <aarindave@gmail.com>
2024-11-09 20:44:12 -0500

    Created script for zoom functionality
    ZoomCamera.cs was created to allow for a camera zoom upon scrolling the mouse.

---
+++ Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    [SerializeField] private int scrollSpeed = 10;
+    [SerializeField] private CinemachineVirtualCamera virtualCamera;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // virtualCamera.Follow.
+    }
+}
