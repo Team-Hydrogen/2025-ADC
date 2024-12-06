f3cfc8b191f3b0620f1ef143040c20abb973afe2
Boomexe <aptenathan@gmail.com>
2024-12-05 19:19:44 -0800

    Excluded moon from post processing, stopped minimap from lagging behind

--- Assets/Scripts/Managers/MinimapManager.cs
+++ Assets/Scripts/Managers/MinimapManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -12,6 +13,8 @@ public class MinimapManager : MonoBehaviour
     
     [Header("Settings")]
     [SerializeField] private float trajectoryScale;
+
+    private int previousIndex = 0;
     
     public static MinimapManager Instance { get; private set; }
 
@@ -78,17 +81,71 @@ public class MinimapManager : MonoBehaviour
 
     private void UpdateMinimapTrajectory(int index)
     {
-        // The current future trajectory is loaded.
-        Vector3[] futureTrajectoryPoints = new Vector3[futureMinimapTrajectory.positionCount];
-        futureMinimapTrajectory.GetPositions(futureTrajectoryPoints);
-        // The past trajectory's list of positions expands, so the next future data point is added.
-        Vector3 nextTrajectoryPoint = futureTrajectoryPoints[1];
-        pastMinimapTrajectory.positionCount++;
-        pastMinimapTrajectory.SetPosition(pastMinimapTrajectory.positionCount - 1, nextTrajectoryPoint);
-        // The next point in the future trajectory gets removed.
-        futureTrajectoryPoints = futureTrajectoryPoints[1..^1];
-        futureMinimapTrajectory.positionCount--;
-        futureMinimapTrajectory.SetPositions(futureTrajectoryPoints);
+        int indexChange = index - previousIndex;
+        previousIndex = index;
+
+        if (indexChange > 0)
+        {
+            Vector3[] futureTrajectoryPoints = new Vector3[futureMinimapTrajectory.positionCount];
+            futureMinimapTrajectory.GetPositions(futureTrajectoryPoints);
+
+            Vector3[] pointsToMove = new Vector3[indexChange];
+            Array.Copy(futureTrajectoryPoints, 0, pointsToMove, 0, indexChange);
+
+            // Add these points to the past trajectory
+            Vector3[] pastTrajectoryPoints = new Vector3[pastMinimapTrajectory.positionCount];
+            pastMinimapTrajectory.GetPositions(pastTrajectoryPoints);
+
+            // Combine past trajectory points and new points
+            Vector3[] newPastTrajectoryPoints = new Vector3[pastTrajectoryPoints.Length + pointsToMove.Length];
+            Array.Copy(pastTrajectoryPoints, 0, newPastTrajectoryPoints, 0, pastTrajectoryPoints.Length);
+            Array.Copy(pointsToMove, 0, newPastTrajectoryPoints, pastTrajectoryPoints.Length, pointsToMove.Length);
+
+            // Update past trajectory
+            pastMinimapTrajectory.positionCount = newPastTrajectoryPoints.Length;
+            pastMinimapTrajectory.SetPositions(newPastTrajectoryPoints);
+
+            // Remove moved points from future trajectory
+            int newFuturePointCount = futureMinimapTrajectory.positionCount - indexChange;
+            Vector3[] newFutureTrajectoryPoints = new Vector3[newFuturePointCount];
+            Array.Copy(futureTrajectoryPoints, indexChange, newFutureTrajectoryPoints, 0, newFuturePointCount);
+
+            futureMinimapTrajectory.positionCount = newFuturePointCount;
+            futureMinimapTrajectory.SetPositions(newFutureTrajectoryPoints);
+        }
+
+        else if (indexChange < 0)
+        {
+            indexChange = -indexChange;
+
+            // Get all points in the past trajectory
+            Vector3[] pastTrajectoryPoints = new Vector3[pastMinimapTrajectory.positionCount];
+            pastMinimapTrajectory.GetPositions(pastTrajectoryPoints);
+
+            // Extract points to move back to the future trajectory
+            Vector3[] pointsToMove = new Vector3[indexChange];
+            Array.Copy(pastTrajectoryPoints, pastTrajectoryPoints.Length - indexChange, pointsToMove, 0, indexChange);
+
+            // Add these points back to the future trajectory
+            Vector3[] futureTrajectoryPoints = new Vector3[futureMinimapTrajectory.positionCount];
+            futureMinimapTrajectory.GetPositions(futureTrajectoryPoints);
+
+            Vector3[] newFutureTrajectoryPoints = new Vector3[futureTrajectoryPoints.Length + pointsToMove.Length];
+            Array.Copy(pointsToMove, 0, newFutureTrajectoryPoints, 0, pointsToMove.Length);
+            Array.Copy(futureTrajectoryPoints, 0, newFutureTrajectoryPoints, pointsToMove.Length, futureTrajectoryPoints.Length);
+
+            // Update future trajectory
+            futureMinimapTrajectory.positionCount = newFutureTrajectoryPoints.Length;
+            futureMinimapTrajectory.SetPositions(newFutureTrajectoryPoints);
+
+            // Remove moved points from past trajectory
+            int newPastPointCount = pastMinimapTrajectory.positionCount - indexChange;
+            Vector3[] newPastTrajectoryPoints = new Vector3[newPastPointCount];
+            Array.Copy(pastTrajectoryPoints, 0, newPastTrajectoryPoints, 0, newPastPointCount);
+
+            pastMinimapTrajectory.positionCount = newPastPointCount;
+            pastMinimapTrajectory.SetPositions(newPastTrajectoryPoints);
+        }
     }
     
     /// <summary>
