df302523f8f4cc2ffefa913e29ee3cef6b7ab2a2
Aarin Dave <aarindave@gmail.com>
2025-03-20 21:43:01 -0400

    Update HttpManager.cs

--- Assets/Scripts/Managers/HttpManager.cs
+++ Assets/Scripts/Managers/HttpManager.cs
@@ -8,7 +8,7 @@ public class HttpManager : MonoBehaviour
 {
     public static HttpManager Instance { get; private set; }
     
-    private const string BumpOffCourseApiUri = "https://5ef6-2601-18c-500-fbb-18f2-2a3b-3c1e-d7bb.ngrok-free.app/trajectory";
+    private const string BumpOffCourseApiUri = "https://ce21-2601-18c-500-fbb-7f8c-2e76-f43d-413a.ngrok-free.app/trajectory";
     private const string BumpOffCourseApiContentType = "application/json";
     
     public static event Action<string> OnPathCalculated;
