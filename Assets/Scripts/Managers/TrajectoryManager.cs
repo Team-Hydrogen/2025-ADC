6e38a8658d7504795345a9f676a2362a2aa64f7c
Aarin Dave <aarindave@gmail.com>
2025-03-23 20:05:04 -0400

    Update TrajectoryManager.cs

--- Assets/Scripts/Managers/TrajectoryManager.cs
+++ Assets/Scripts/Managers/TrajectoryManager.cs
@@ -71,83 +71,14 @@ public class TrajectoryManager : MonoBehaviour
     }
     
     /// <summary>
-    /// Sets the past trajectory based on stage
-    /// </summary>
-    /// <param name="stage">Updated mission stage</param>
-    private void SetPastTrajectoryRendererByStage(MissionStage stage)
-    {
-        // The past trajectory line renderer is set to the mission stage's respective line renderer.
-        if (_pastNominalTrajectoryLineRenderer.Equals(stage.nominalLineRenderer))
-        {
-            return;
-        }
-        
-        _pastNominalTrajectoryLineRenderer = stage.nominalLineRenderer;
-        _pastNominalTrajectoryLineRenderer = stage.offNominalLineRenderer;
-        
-        // _pastNominalTrajectoryLineRenderer.SetPosition(0, NominalSpacecraftTransform.position);
-        // _pastNominalTrajectoryLineRenderer.SetPosition(0, OffNominalSpacecraftTransform.position);
-        
-        // trigger animation here if it is correct stage
-    }
-    
-    /// <summary>
     /// Visualizes provided trajectory data
-    /// </summary>
-    /// <param name="points">A list of three-dimensional points as strings</param>
-    /// <param name="past">The line renderer visualizing the past trajectory</param>
-    /// <param name="future">The line renderer visualizing the future trajectory</param>
-    private void PlotTrajectory(List<string[]> points, LineRenderer past, LineRenderer future)
-    {
-        // An array of three-dimensional points is constructed by processing the CSV file.
-        int numberOfPoints = points.Count;
-        Vector3[] futurePoints = new Vector3[numberOfPoints];
-        
-        for (int index = 0; index < numberOfPoints; index++)
-        {
-            string[] point = points[index];
-            
-            try
-            {
-                Vector3 pointAsVector = new Vector3(
-                    float.Parse(point[1]) * trajectoryScale,
-                    float.Parse(point[2]) * trajectoryScale,
-                    float.Parse(point[3]) * trajectoryScale);
-                
-                futurePoints[index] = pointAsVector;
-            }
-            catch
-            {
-                Debug.LogWarning($"No positional data exists on line {index}!");
-            }
-        }
-        
-        // The past trajectory has two points. The first point in the past trajectory represents the first given data
-        // point. The second data point represents the spacecraft's current position. Upon initialization, the
-        // spacecraft will begin at the first data point. Therefore, both points in the past trajectory are set to the
-        // first data point.
-        past.positionCount = 2;
-        past.SetPosition(0, futurePoints[0]); // The first provided data point
-        past.SetPosition(1, futurePoints[0]); // The spacecraft's current position
-        
-        // The future trajectory has N points. The first point in the future trajectory represents the spacecraft's
-        // current position. The last data point of the past trajectory must be the first data point of the spacecraft
-        // to ensure both ends of the trajectory connect to the spacecraft. Upon initialization, the spacecraft will
-        // begin at the first data point. The spacecraft would have not visited any data points beyond the first data
-        // point. Therefore, the remaining data points are pushed to the future trajectory. 
-        future.positionCount = numberOfPoints;
-        future.SetPositions(futurePoints);
-    }
-    
-    /// <summary>
-    /// Splits the trajectory into past and future halves based on a provided time
     /// </summary>
     /// <param name="currentTime">The provided simulation time</param>
     /// <param name="trajectoryStageCutoffs"></param>
     /// <param name="trajectoryData"></param>
     /// <param name="trajectory"></param>
-    private void SplitTrajectory(float currentTime, float[] trajectoryStageCutoffs,
-        List<string[]> trajectoryData, Transform trajectory)
+    private void PlotTrajectory(float currentTime, float[] trajectoryStageCutoffs, List<string[]> trajectoryData, 
+        Transform trajectory)
     {
         // The total amount of data points and sub-trajectories are determined. The total number of sub-trajectories
         // includes multiple past trajectories and the designated future trajectory.  
