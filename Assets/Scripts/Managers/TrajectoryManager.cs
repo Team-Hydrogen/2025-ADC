91eb77d15022dc6e6e4e8b8994baf0dd985cff85
Aarin Dave <aarindave@gmail.com>
2025-03-23 20:01:51 -0400

    Significant revisions to trajectory algorithm

--- Assets/Scripts/Managers/TrajectoryManager.cs
+++ Assets/Scripts/Managers/TrajectoryManager.cs
@@ -143,72 +143,67 @@ public class TrajectoryManager : MonoBehaviour
     /// Splits the trajectory into past and future halves based on a provided time
     /// </summary>
     /// <param name="currentTime">The provided simulation time</param>
-    /// <param name="data">The trajectory data</param>
-    /// <param name="past">The line renderer visualizing the past trajectory</param>
-    /// <param name="future">The line renderer visualizing the future trajectory</param>
-    /// <param name="errorLines">The number of trailing erroneous or null lines</param>
-    /// <returns>The spacecraft's interpolated position</returns>
-    private Vector3 SplitTrajectory(float currentTime, List<string[]> data, LineRenderer past, LineRenderer future, int errorLines=3)
+    /// <param name="trajectoryStageCutoffs"></param>
+    /// <param name="trajectoryData"></param>
+    /// <param name="trajectory"></param>
+    private void SplitTrajectory(float currentTime, float[] trajectoryStageCutoffs,
+        List<string[]> trajectoryData, Transform trajectory)
     {
-        // The number of data points is calculated ahead of time based on the number of erroneous lines.
-        int numberOfValidPoints = data.Count - errorLines;
+        // The total amount of data points and sub-trajectories are determined. The total number of sub-trajectories
+        // includes multiple past trajectories and the designated future trajectory.  
+        int totalDataPoints = trajectoryData.Count;
+        int totalSubTrajectories = trajectory.childCount; // The final sub-trajectory is always the future trajectory.
         
-        // The pivot index is the first index of the dataset where the time is ahead of the provided `currentTime`.
-        int pivotIndex = 1;
-        float lowerTime = float.Parse(data[pivotIndex - 1][0]);
-        float upperTime = float.Parse(data[pivotIndex][0]);
-        
-        while (lowerTime >= currentTime && currentTime < upperTime && pivotIndex < numberOfValidPoints)
+        List<Vector3>[] subTrajectoryPositions = new List<Vector3>[totalSubTrajectories];
+        int subTrajectoryIndex = 0;
+
+        for (int dataIndex = 0; dataIndex < totalDataPoints; dataIndex++)
         {
-            lowerTime = float.Parse(data[pivotIndex - 1][0]);
-            upperTime = float.Parse(data[pivotIndex][0]);
-            pivotIndex++;
+            string[] dataLine = trajectoryData[dataIndex];
+
+            float time;
+            Vector3 currentPoint;
+            
+            try
+            {
+                time = float.Parse(dataLine[0]);
+                currentPoint = new Vector3(
+                    float.Parse(dataLine[1]), float.Parse(dataLine[2]), float.Parse(dataLine[3]));
+            }
+            catch
+            {
+                Debug.LogWarning($"Neither temporal nor positional trajectory data exists on data line {dataIndex}.");
+                break;
+            }
+            
+            if (time >= trajectoryStageCutoffs[subTrajectoryIndex] || time > currentTime)
+            {
+                float interpolationRatio = Mathf.InverseLerp(
+                    float.Parse(trajectoryData[dataIndex - 1][0]), time, currentTime);
+                
+                Vector3 interpolatedPoint = new Vector3(
+                    Mathf.Lerp(currentPoint.x, float.Parse(trajectoryData[dataIndex - 1][1]), interpolationRatio),
+                    Mathf.Lerp(currentPoint.y, float.Parse(trajectoryData[dataIndex - 1][2]), interpolationRatio),
+                    Mathf.Lerp(currentPoint.z, float.Parse(trajectoryData[dataIndex - 1][3]), interpolationRatio)
+                );
+                
+                subTrajectoryPositions[subTrajectoryIndex].Add(interpolatedPoint);
+                subTrajectoryIndex = time > currentTime ? totalSubTrajectories - 1 : subTrajectoryIndex + 1;
+                subTrajectoryPositions[subTrajectoryIndex].Add(interpolatedPoint);
+            }
+
+            subTrajectoryPositions[subTrajectoryIndex].Add(currentPoint);
         }
         
-        // The interpolation ratio is found between the two times, which will later be applied to positions.
-        float interpolationRatio = Mathf.InverseLerp(lowerTime, upperTime, currentTime);
-        // The spacecraft's current position is calculated.
-        Vector3 interpolatedPoint = new Vector3(
-            Mathf.Lerp(float.Parse(data[pivotIndex - 1][1]), float.Parse(data[pivotIndex][1]), interpolationRatio),
-            Mathf.Lerp(float.Parse(data[pivotIndex - 1][2]), float.Parse(data[pivotIndex][2]), interpolationRatio),
-            Mathf.Lerp(float.Parse(data[pivotIndex - 1][3]), float.Parse(data[pivotIndex][3]), interpolationRatio)
-        ) * trajectoryScale;
-        
-        // The number of points in the past trajectory and future trajectory are calculated and set.
-        past.positionCount = pivotIndex + 1;
-        future.positionCount = numberOfValidPoints - pivotIndex + 2;
-        
-        // The points in the past trajectory are determined by including all positions before the pivot index. The
-        // points in the future trajectory are determined by including all positions at and after the pivot index. The
-        // interpolated position is included as the past trajectory's final data point and the future trajectory's
-        // first data point to visually connect the trajectory.
-        Vector3[] pastTrajectoryPoints = new Vector3[pivotIndex + 1];
-        Vector3[] futureTrajectoryPoints = new Vector3[numberOfValidPoints - pivotIndex + 2];
-        
-        for (int dataIndex = 0; dataIndex < pivotIndex; dataIndex++)
+        // Assign each sub-trajectory their respective positions.
+        for (int trajectoryIndex = 0; trajectoryIndex < subTrajectoryPositions.Length; trajectoryIndex++)
         {
-            pastTrajectoryPoints[dataIndex] = new Vector3(
-                float.Parse(data[dataIndex][1]),
-                float.Parse(data[dataIndex][2]),
-                float.Parse(data[dataIndex][3])
-            ) * trajectoryScale;
+            LineRenderer subTrajectoryRenderer = trajectory.GetChild(trajectoryIndex).GetComponent<LineRenderer>();
+            
+            Vector3[] positions = subTrajectoryPositions[trajectoryIndex].ToArray();
+            subTrajectoryRenderer.positionCount = positions.Length;
+            subTrajectoryRenderer.SetPositions(positions);
         }
-        for (int dataIndex = pivotIndex; dataIndex < numberOfValidPoints; dataIndex++)
-        {
-            futureTrajectoryPoints[dataIndex - pivotIndex + 1] = new Vector3(
-                float.Parse(data[dataIndex][1]),
-                float.Parse(data[dataIndex][2]),
-                float.Parse(data[dataIndex][3])
-            ) * trajectoryScale;
-        }
-        
-        pastTrajectoryPoints[^1] = interpolatedPoint; // The past trajectory's final data point
-        futureTrajectoryPoints[0] = interpolatedPoint; // The future trajectory's first data point
-        
-        past.SetPositions(pastTrajectoryPoints);
-        future.SetPositions(futureTrajectoryPoints);
-        
-        return interpolatedPoint;
     }
     
     #endregion
