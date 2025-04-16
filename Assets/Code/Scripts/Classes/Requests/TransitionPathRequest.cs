fea6c40d5dec531ccafa5d1a78e496f1d02f79e9
Aarin Dave <aarindave@gmail.com>
2025-04-16 18:16:49 -0500

    Intelligence fixes

--- Assets/Code/Scripts/Classes/Requests/TransitionPathRequest.cs
+++ Assets/Code/Scripts/Classes/Requests/TransitionPathRequest.cs
@@ -6,55 +6,48 @@ public class TransitionPathRequest
     public float[] OriginVelocity;
 
     public float[] DestinationPosition;
-    public float[] DestinationVelocity;
 
     public float StartTime;
     public float FlightTime;
-    
+
     /// <summary>
     /// Parameter constructor for TransitionPathRequest
     /// </summary>
     /// <param name="originPosition">The original position</param>
     /// <param name="originVelocity">The original velocity</param>
     /// <param name="destinationPosition">The destination position</param>
-    /// <param name="destinationVelocity">The destination velocity</param>
     /// <param name="startTime">The initial time</param>
     /// <param name="flightTime">The expected flight time</param>
     public TransitionPathRequest(
-        float[] originPosition, float[] originVelocity, float[] destinationPosition, float[] destinationVelocity,
-        float startTime, float flightTime)
+        float[] originPosition, float[] originVelocity, float[] destinationPosition, float startTime, float flightTime)
     {
         OriginPosition = originPosition;
         OriginVelocity = originVelocity;
         DestinationPosition = destinationPosition;
-        DestinationVelocity = destinationVelocity;
         StartTime = startTime;
         FlightTime = flightTime;
     }
-    
+
     /// <summary>
     /// Converts required data for TransitionPathRequest into JSON format
     /// </summary>
     /// <param name="originPosition">The original position</param>
     /// <param name="originVelocity">The original velocity</param>
     /// <param name="destinationPosition">The destination position</param>
-    /// <param name="destinationVelocity">The destination velocity</param>
     /// <param name="startTime">The initial time</param>
     /// <param name="flightTime">The expected flight time</param>
     /// <returns>A JSON-formatted string with information</returns>
     public static string ToJson(Vector3 originPosition, Vector3 originVelocity, Vector3 destinationPosition,
-        Vector3 destinationVelocity, float startTime, float flightTime)
+        float startTime, float flightTime)
     {
         float[] originPositionPostData = { originPosition.x, originPosition.y, originPosition.z };
         float[] originVelocityPostData = { originVelocity.x, originVelocity.y, originVelocity.z };
         float[] destinationPositionPostData = { destinationPosition.x, destinationPosition.y, destinationPosition.z };
-        float[] destinationVelocityPostData = { destinationVelocity.x, destinationVelocity.y, destinationVelocity.z };
-        
+
         TransitionPathRequest apiRequest = new TransitionPathRequest(
             originPositionPostData,
             originVelocityPostData,
             destinationPositionPostData,
-            destinationVelocityPostData,
             startTime,
             flightTime
         );
---
+++ Assets/Code/Scripts/Utilities/MoveMoon.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMoon : MonoBehaviour
+{
+    
+    
+    private void OnEnable()
+    {
+        // DataManager.DataIndexUpdated
+    }
+
+    private void OnDisable()
+    {
+        // DataManager.DataIndexUpdated
+    }
+}
